d12305d2a83200b99658c3336d2710db1625a524
San'Quan Prioleau <sq.prioleau@gmail.com>
2024-06-08 11:55:58 -0400

    💄 Update timer styles

--- src/components/GameInfoBar/index.tsx
+++ src/components/GameInfoBar/index.tsx
@@ -37,7 +37,9 @@ export default function GameInfoBar({
 							data-should-pulse={isInFinalSeconds ? "true" : "false"}
 							data-is-paused={isRunning ? "false" : "true"}
 						>
-							{`${String(minutes).padStart(2, "0")}:${String(seconds).padStart(2, "0")}`}
+							<span>{`${String(minutes).padStart(2, "0")}`}</span>
+							<span>:</span>
+							<span>{`${String(seconds).padStart(2, "0")}`}</span>
 						</span>
 					</OutlinedText>
 				</section>
