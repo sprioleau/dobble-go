f3ec237a0218d59a23809454072abc02f1a73300
San'Quan Prioleau <sq.prioleau@gmail.com>
2024-06-09 10:17:52 -0400

    💄 Fix styling on "How to Play" page

--- src/app/how-to-play/page.tsx
+++ src/app/how-to-play/page.tsx
@@ -3,7 +3,16 @@ import styles from "./page.module.scss";
 import OutlinedText from "@/components/OutlinedText";
 import BackToMenuPageLink from "@/components/BackToMenuPageLink";
 
+const instructions = [
+	"Find the symbol that exists on both cards. There is only 1.",
+	"Select that symbol on either card to reveal the next card.",
+	"Continue selecting symbols until there are no more cards left.",
+	"Selecting the wrong symbol will remove a point from your score, so be as fast an accurate as possible.",
+];
+
 export default function HowToPlayPage() {
+	const fontSize = "2rem";
+
 	return (
 		<main
 			className={styles.main}
@@ -11,20 +20,14 @@ export default function HowToPlayPage() {
 		>
 			<Logo />
 			<ol className={styles.list}>
-				<li>
-					<OutlinedText>Find the symbol that exists on both cards. There is only 1.</OutlinedText>
-				</li>
-				<li>
-					<OutlinedText>Select that symbol on either card to reveal the next card.</OutlinedText>
-				</li>
-				<li>
-					<OutlinedText>Continue selecting symbols until there are no more cards left.</OutlinedText>
-				</li>
-				<li>
-					<OutlinedText>
-						Selecting the wrong symbol will remove a point from your score, so be as fast an accurate as possible.
-					</OutlinedText>
-				</li>
+				{instructions.map((instruction) => (
+					<li
+						key={instruction}
+						style={{ fontSize }}
+					>
+						<OutlinedText fontSize={fontSize}>{instruction}</OutlinedText>
+					</li>
+				))}
 			</ol>
 			<BackToMenuPageLink />
 		</main>
