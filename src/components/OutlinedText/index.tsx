efc0f6d6542776cb59637d14b8f50d1ae1680e0f
San'Quan Prioleau <sq.prioleau@gmail.com>
2024-06-09 10:05:07 -0400

    ✨ Allow passing in `fontSize` to `OutlinedText` component

--- src/components/OutlinedText/index.tsx
+++ src/components/OutlinedText/index.tsx
@@ -1,9 +1,17 @@
 import styles from "./index.module.scss";
 
 type Props = {
+	fontSize: `${number}${"px" | "em" | "rem"}` | number;
 	children: React.ReactNode;
 };
 
-export default function OutlinedText({ children }: Props) {
-	return <span className={styles["outlined-text"]}>{children}</span>;
+export default function OutlinedText({ fontSize = "2.75rem", children }: Props) {
+	return (
+		<span
+			className={styles["outlined-text"]}
+			style={{ "--font-size": fontSize } as React.CSSProperties}
+		>
+			{children}
+		</span>
+	);
 }
